e60939240637b3eb58880eb28335e0df70d5a103
Ayman Talat <aymantalat2@gmail.com>
2024-11-24 12:56:20 -0600

    Added banner feature for Restaurant opening hours

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,32 @@
 import NavBar from "../components/NavBar"
 import HomePageImg from "../assets/img/homepage-img.jpg"
+import { useState, useEffect } from "react"
+
+function isRestaurantOpen() {
+  const now = new Date();
+  const day = now.getUTCDay(); // Day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+  const hour = now.getHours(); // 0 -> 23
+  if (day <= 4 && hour >= 9 && hour <= 23) {
+    return true;
+  } 
+  return false;
+}
 
 function HomePage() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setIsOpen(isRestaurantOpen());
+  }, []);
+
   return (
     <>
       <NavBar/>
+      <p className="banner" style={{ display: isOpen ? "block" : "none" }}>Restaurant is Open now!</p>
       <div id="content">
-        <h1 className="homepage-headline">Ayman's Restaurant</h1>
+        <h1 className="homepage-headline">Ayman&apos;s Restaurant</h1>
         <div className="homepage-desc">
-        Welcome to "Ayman's Restaurant" a modern yet cozy restaurant in the heart of the city, where culinary artistry meets fresh, locally sourced ingredients. Our diverse menu, inspired by both local traditions and global flavors, offers something for every palate—from sizzling steaks to delicate sushi rolls. Whether you're here for a quick bite or a romantic dinner, our warm atmosphere and attentive service promise an unforgettable dining experience.
+        Welcome to &quot;Ayman&apos;s Restaurant&quot; a modern yet cozy restaurant in the heart of the city, where culinary artistry meets fresh, locally sourced ingredients. Our diverse menu, inspired by both local traditions and global flavors, offers something for every palate—from sizzling steaks to delicate sushi rolls. Whether you're here for a quick bite or a romantic dinner, our warm atmosphere and attentive service promise an unforgettable dining experience.
         </div>
         <img className="homepage-img" src={HomePageImg} alt="Interior of cozy restaurant, loft style stock photo"></img>
       </div>
